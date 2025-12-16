f9b3926de40e952b8da2d18811693d3ccbe699b5
elotex0 <justin.kettner14@icloud.com>
2025-12-16 02:49:32 +0100

    Update geo.py

--- downloads/geo.py
+++ downloads/geo.py
@@ -9,7 +9,7 @@ time = int(os.getenv("RUN", 0)) # z. B. 0, 12, etc.
 date = f"{date[:4]}-{date[4:6]}-{date[6:8]}"
 
 # --- Zielordner je nach Variable ---
-output_dir = "data/t2m"  # in msl.py oder ptype.py anpassen
+output_dir = "data/geo"  # in msl.py oder ptype.py anpassen
 os.makedirs(output_dir, exist_ok=True)
 
 # ECMWF Open Data Client (AWS Mirror empfohlen)
@@ -19,7 +19,7 @@ client = Client(source="aws")
 steps = list(range(0, 361, 6))
 
 for step in steps:
-    filename = f"2t_step_{step:03d}.grib2"  # im jeweiligen Script anpassen
+    filename = f"geo_step_{step:03d}.grib2"  # im jeweiligen Script anpassen
     target_path = os.path.join(output_dir, filename)
 
     print(f"Lade Step +{step:03d}h für {date} {time:02d} UTC → {target_path}")
